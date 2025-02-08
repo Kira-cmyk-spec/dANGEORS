e7e358df70be75033411e77302ea0374da49351c
Georjia <Georjia@DESKTOP-EDLOQQ8>
2025-02-08 14:53:44 +0300

    Добавилось ограниченное использование зелий и их покупка. За победу над монстром, тоже дают деньги

--- dANGEORS/Program.cs
+++ dANGEORS/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System;
+using static System.Net.Mime.MediaTypeNames;
 
 class Program
 {
@@ -29,20 +30,7 @@ class Program
             }
         }
         dungeonMap[9] = "Boss"; // Босс в 10-й комнате
-        static int Merchant(int playerGold)
-        {
-            Console.WriteLine("Торговец предлагает зелье за 30 золота.");
-            if (playerGold >= 30)
-            {
-                playerGold -= 30;
-                Console.WriteLine("Вы купили зелье.");
-            }
-            else
-            {
-                Console.WriteLine("У Вас недостаточно золота.");
-            }
-            return playerGold;
-        }
+
         static int OpenChest(int playerGold)
         {
             Random random = new Random();
@@ -50,7 +38,7 @@ class Program
             // Генерация двух случайных чисел
             int number1 = random.Next(0, 101);
             int number2 = random.Next(0, 101);
-            int Gold = 0;
+           
 
             // Формирование задачи
             Console.WriteLine($"{number1} + {number2} = ?");
@@ -65,8 +53,8 @@ class Program
                 // Проверка правильности ответа
                 if (userAnswer == number1 + number2)
                 {
-                    playerGold =Gold  + 30;
-                    Console.WriteLine($"Вы открыли сундук и нашли 20 золото!" + "У вас сейчас золота: "+ playerGold);
+                    playerGold +=  30;
+                    Console.WriteLine($"Вы открыли сундук и нашли 30 золото!" + "\nУ вас сейчас золота: " + playerGold);
 
                 }
                 else
@@ -79,6 +67,159 @@ class Program
             return playerGold;
 
         }
+        static int Merchant(int playerGold, ref int inventoryCount)
+        {
+            Console.WriteLine($"Торговец предлагает зелье Лечения за 30 золота. \n У вас сейчас {inventoryCount} зелий и сейчас золота: {playerGold}");
+            Console.WriteLine(" 1 - Купить зелье , 2 - Отказаться");
+            int weaponChoice = int.Parse(Console.ReadLine());
+            if (weaponChoice == 1)
+            {
+                if (playerGold >= 30)
+                {
+                    playerGold -= 30;
+                    inventoryCount += 1;
+                    Console.WriteLine($"Вы купили зелье.\n У вас {inventoryCount} зелий  и {playerGold} денег");
+                }
+                else
+                {
+                    Console.WriteLine("У Вас недостаточно золота.");
+                }
+
+            }
+            else if (weaponChoice == 2)
+            {
+                Console.WriteLine("Вы отказались");
+            }
+
+            return playerGold;
+        }
+
+        static int BattleWithMonster1(int playerHealth, ref int inventoryCount)
+        {
+            Random random = new Random();
+            int monsterHealth = random.Next(51, 80);
+            Console.WriteLine($"Вы встретили Босса с {monsterHealth} HP!");
+            Console.WriteLine($"\n У вас {inventoryCount} зелий ");
+
+            while (playerHealth > 0 && monsterHealth > 0)
+            {
+              
+                Console.WriteLine("Выберите оружие: 1 - Меч, 2 - Лук, 3 - Использовать Зелье Лечения");
+                int weaponChoice = int.Parse(Console.ReadLine());
+                int damage = 0;
+                int playerHealth1 = 0;
+                int playerHealth2 = 0;
+
+                if (weaponChoice == 1) // Меч
+                {
+                    damage = random.Next(10, 25);
+                }
+                else if (weaponChoice == 2) // Лук
+                {
+                    // Проверка наличия стрел
+                    // Если стрел нет, то нельзя использовать лук
+                    damage = random.Next(5, 20);
+                }
+                else if (weaponChoice == 3 && inventoryCount > 0) // Зелье Лечения
+                {
+                   
+                       
+                        playerHealth1 = random.Next(15, 30);
+                        playerHealth2 = playerHealth + playerHealth1; 
+                        Console.WriteLine($"Вы добавили себе {playerHealth1} HP. Осталось здоровье: {playerHealth2}");
+                        inventoryCount--;
+                    
+
+                }
+                else
+                {
+                    Console.WriteLine("Вы бездействовали");
+                }
+
+                monsterHealth -= damage;
+                Console.WriteLine($"Вы нанесли {damage} урона Боссу. Осталось HP: {monsterHealth}");
+
+                if (monsterHealth > 0)
+                {
+                    int monsterDamage = random.Next(5, 16);
+                    if (weaponChoice == 3) // Зелье Лечения
+                    {
+                        playerHealth = playerHealth2;
+                    }
+                    playerHealth -= monsterDamage;
+                    Console.WriteLine($"Монстр атакует! Вы потеряли {monsterDamage} HP. Осталось здоровье: {playerHealth}");
+                }
+            }
+
+            return playerHealth;
+            
+        }
+
+
+        static int BattleWithMonster(int playerHealth, ref int inventoryCount)
+        {
+            Random random = new Random();
+            int monsterHealth = random.Next(20, 51);
+            Console.WriteLine($"Вы встретили монстра с {monsterHealth} HP!");
+            Console.WriteLine($"\n У вас {inventoryCount} зелий и {playerHealth} HP ");           
+
+            while (playerHealth > 0 && monsterHealth > 0)
+            {
+
+                Console.WriteLine("Выберите оружие: 1 - Меч, 2 - Лук, 3 - Использовать Зелье Лечения");
+                int weaponChoice = int.Parse(Console.ReadLine());
+                int damage = 0;
+                int playerHealth1 = 0;
+                int playerHealth2 = 0;
+                if (weaponChoice == 1) // Меч
+                {
+                    damage = random.Next(10, 21);
+                }
+                else if (weaponChoice == 2) // Лук
+                {
+                    // Проверка наличия стрел
+                    // Если стрел нет, то нельзя использовать лук
+                    damage = random.Next(5, 16);
+                }
+                else if (weaponChoice == 3 && inventoryCount > 0) // Зелье Лечения
+                {
+                   
+                       
+                        playerHealth1 = random.Next(15, 30);
+                        playerHealth2 = playerHealth + playerHealth1;
+                        inventoryCount -= 1;
+                        Console.WriteLine($"Вы добавили себе {playerHealth1} HP. Осталось здоровье: {playerHealth2} \n зелий у вас осталось {inventoryCount}" );
+                        
+                    
+                    
+
+                }
+                else
+                    {
+                        Console.WriteLine("Вы бездействовали");
+                    }
+                monsterHealth -= damage;
+                Console.WriteLine($"Вы нанесли {damage} урона монстру. Осталось HP: {monsterHealth}");
+
+                if (monsterHealth > 0)
+                {
+                    int monsterDamage = random.Next(5, 16);
+                    if (weaponChoice == 3) // Зелье Лечения
+                    {
+                        playerHealth = playerHealth2;
+                    }
+
+                    playerHealth -= monsterDamage;
+
+                    Console.WriteLine($"Монстр атакует! Вы потеряли {monsterDamage} HP. Осталось здоровье: {playerHealth}");
+                }
+
+
+
+            }
+            return playerHealth;
+        }
+
         // Игровой цикл
         for (int room = 0; room < dungeonMap.Length; room++)
         {
@@ -87,8 +228,11 @@ class Program
             switch (dungeonMap[room])
             {
                 case "Monster":
-                    playerHealth = BattleWithMonster(playerHealth);
-                    if (playerHealth <= 0) return; // Конец игры
+                    playerHealth = BattleWithMonster(playerHealth, ref inventoryCount);
+                    if (playerHealth <= 0) return;// Конец игры
+
+                    playerGold +=  30; // Конец рунда
+                    Console.WriteLine($"Вы победили монстра.\n Вы нашли у него 30 золота \n У вас {inventoryCount} зелий  и {playerGold} денег");
                     break;
 
                 case "Trap":
@@ -98,11 +242,11 @@ class Program
                     break;
 
                 case "Chest":
-                     playerGold = OpenChest(playerGold);
+                    playerGold = OpenChest(playerGold);
                     break;
 
                 case "Merchant":
-                    playerGold = Merchant(playerGold);
+                    playerGold = Merchant(playerGold, ref  inventoryCount);
                     break;
 
                 case "Empty":
@@ -111,115 +255,15 @@ class Program
 
                 case "Boss":
                     Console.WriteLine("Вы встретили босса!");
-                    playerHealth = BattleWithMonster1(playerHealth);
-                    if (playerHealth <= 0) return; // Конец игры // Логика боя с боссом
+                    playerHealth = BattleWithMonster1(playerHealth, ref inventoryCount);
+                    if (playerHealth <= 0) return;
+                    
+                    Console.WriteLine($"Вы победили Босса.\n  У вас осталось {inventoryCount} зелий  и {playerGold} денег");// Конец игры // Логика боя с боссом
                     break;
             }
+
         }
     }
-    static int BattleWithMonster1(int playerHealth)
-    {
-        Random random = new Random();
-        int monsterHealth = random.Next(51, 80);
-        Console.WriteLine($"Вы встретили Босса с {monsterHealth} HP!");
-
-        while (playerHealth > 0 && monsterHealth > 0)
-        {
-            Console.WriteLine("Выберите оружие: 1 - Меч, 2 - Лук");
-            int weaponChoice = int.Parse(Console.ReadLine());
-            int damage = 0;
-            int playerHealth1 = 0;
-            int playerHealth2 = 0;
-
-            if (weaponChoice == 1) // Меч
-            {
-                damage = random.Next(10, 25);
-            }
-            else if (weaponChoice == 2) // Лук
-            {
-                // Проверка наличия стрел
-                // Если стрел нет, то нельзя использовать лук
-                damage = random.Next(5, 20);
-            }
-            else if (weaponChoice == 3) // Зелье Лечения
-            {
-                playerHealth1 = random.Next(5, 20);
-                playerHealth2 = playerHealth + playerHealth1;
-                Console.WriteLine($"Вы добавили себе {playerHealth1} HP. Осталось здоровье: {playerHealth2}");
-
-            }
-
-            monsterHealth -= damage;
-            Console.WriteLine($"Вы нанесли {damage} урона Боссу. Осталось HP: {monsterHealth}");
-
-            if (monsterHealth > 0)
-            {
-                int monsterDamage = random.Next(5, 16);
-                if (weaponChoice == 3) // Зелье Лечения
-                {
-                    playerHealth = playerHealth2;
-                }
-                playerHealth -= monsterDamage;
-                Console.WriteLine($"Монстр атакует! Вы потеряли {monsterDamage} HP. Осталось здоровье: {playerHealth}");
-            }
-        }
-
-        return playerHealth;
-    }
-
-
-    static int BattleWithMonster(int playerHealth)
-    {
-        Random random = new Random();
-        int monsterHealth = random.Next(20, 51);
-        Console.WriteLine($"Вы встретили монстра с {monsterHealth} HP!");
-                 
-               
-        while (playerHealth > 0 && monsterHealth > 0)
-        {
-            Console.WriteLine("Выберите оружие: 1 - Меч, 2 - Лук, 3 - Использовать Зелье Лечения");
-            int weaponChoice = int.Parse(Console.ReadLine());
-            int damage = 0;
-            int playerHealth1 = 0;
-            int playerHealth2 = 0;
-            if (weaponChoice == 1) // Меч
-            {
-                damage = random.Next(10, 21);
-            }
-            else if (weaponChoice == 2) // Лук
-            {
-                // Проверка наличия стрел
-                // Если стрел нет, то нельзя использовать лук
-                damage = random.Next(5, 16);
-            }
-            else if (weaponChoice == 3) // Зелье Лечения
-            {
-                playerHealth1 = random.Next(5, 20); 
-                playerHealth2 = playerHealth + playerHealth1;
-                Console.WriteLine($"Вы добавили себе {playerHealth1} HP. Осталось здоровье: {playerHealth2}");
-
-            }
-                monsterHealth -= damage;
-                Console.WriteLine($"Вы нанесли {damage} урона монстру. Осталось HP: {monsterHealth}");
-              
-                if (monsterHealth > 0)
-                {
-                    int monsterDamage = random.Next(5, 16);
-                if (weaponChoice == 3) // Зелье Лечения
-                {   
-                    playerHealth = playerHealth2;              
-                }
-               
-                playerHealth -= monsterDamage;
-                    
-                    Console.WriteLine($"Монстр атакует! Вы потеряли {monsterDamage} HP. Осталось здоровье: {playerHealth}");
-                }
-            
-
-            
-        }
-       return playerHealth;
-    }
-
-    } 
+}
+    
 
